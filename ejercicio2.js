3124c5b53ced31adca55bb09822d0c00b8c8bbca
nicolas muñoz <nicomo19@gmail.com>
2022-02-01 22:05:35 -0300

    tareas finalizadas

---
+++ ejercicio2.js
@@ -0,0 +1,45 @@
+// boton 1 
+var cambiocaja = document.getElementById("caja")
+var colorbtn = document.getElementById("btn-1")
+
+colorbtn.addEventListener("click", ()=>{
+    cambiocaja.style.backgroundColor = "#e53e3e"
+})
+//boton 2
+var cambiocaja = document.getElementById("caja")
+var colorbtn = document.getElementById("btn-2")
+
+colorbtn.addEventListener("click", ()=>{
+    cambiocaja.style.backgroundColor = "#dd6b20"
+})
+//boton 3
+var cambiocaja = document.getElementById("caja")
+var colorbtn = document.getElementById("btn-3")
+
+colorbtn.addEventListener("click", ()=>{
+    cambiocaja.style.backgroundColor = "#faf089"
+})
+// boton 4
+var cambiocaja = document.getElementById("caja")
+var colorbtn = document.getElementById("btn-4")
+
+colorbtn.addEventListener("click", ()=>{
+    cambiocaja.style.backgroundColor = "#48bb78"
+})
+//boton 5
+var cambiocaja = document.getElementById("caja")
+var colorbtn = document.getElementById("btn-5")
+
+colorbtn.addEventListener("click", ()=>{
+    cambiocaja.style.backgroundColor = "#81e6d9"
+})
+//boton 6
+var cambiocaja = document.getElementById("caja")
+var colorbtn = document.getElementById("btn-6")
+
+colorbtn.addEventListener("click", ()=>{
+    cambiocaja.style.backgroundColor = "#d53f8c"
+})
+
+
+
